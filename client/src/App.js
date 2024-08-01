6bfdbc020a726b7578f8e322ac4c9aed70f6ed28
raikarkritesh <raikarkritesh@gmail.com>
2024-07-31 22:28:30 -0400

    Add popups on marker click

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import Map, { Marker } from 'react-map-gl';
+import Map, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { listLogEntries } from './API';
-//require('dotenv').config();
 
 const App = () => {
-  const [logEntries, setLogEntries] = useState([])
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
 
   useEffect(() => {
     (async () => {
@@ -18,7 +18,7 @@ const App = () => {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Map
-        mapboxAccessToken = {process.env.REACT_APP_MAPBOX_TOKEN}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         initialViewState={{
           longitude: -100,
           latitude: 40,
@@ -28,10 +28,41 @@ const App = () => {
         style={{ width: '100%', height: '100%' }}
       >
         {logEntries.map(entry => (
-          <Marker key={entry._id} longitude={entry.longitude} latitude={entry.latitude} anchor="bottom">
+          <React.Fragment key={entry._id}>
+            <Marker
+              key={entry._id}
+              longitude={entry.longitude}
+              latitude={entry.latitude}
+              anchor="bottom"
+              onClick={() => setShowPopup({
+                // ...showPopup,
+                [entry._id]: true,
+              })}
+            >
+            </Marker>
+            {
+              showPopup[entry._id] ? (
+                <Popup
+                  latitude={entry.latitude}
+                  longitude={entry.longitude}
+                  closeOnClick={false}
+                  onClose={() => setShowPopup({})}
+                  anchor="top" >
+                  <div className="popup">
+                    <h3>{entry.title}</h3>
+                    <p>{entry.comments}</p>
+                    <small>Visited on: {new Date(entry.visitDate).toLocaleDateString()}</small>
+                    {entry.image && <img src={entry.image} alt={entry.title} />}
+                  </div>
+                </Popup>
+              ) : null
+            }
 
-          </Marker>
+          </React.Fragment>
+
         ))}
+
+
       </Map>
     </div>
   );
